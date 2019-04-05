ce31a77ed78772efce8c72d6ce5ea5884e71ecdd
Andrea <Andrea@LAPTOP-1JFI8B43>
2019-04-05 19:49:41 +0200

    version 3-estructura de paquetes

---
+++ Proyecto/src/main/java/com/telefonica/Proyecto/config/InitializerConfig.java
@@ -0,0 +1,31 @@
+package com.telefonica.Proyecto.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class InitializerConfig implements WebApplicationInitializer {
+	
+	@Override
+	public void onStartup(ServletContext container) throws ServletException {
+	
+		AnnotationConfigWebApplicationContext context;
+		context = new AnnotationConfigWebApplicationContext();
+		
+		context.setConfigLocation("com.telefonica.Proyecto");
+		container.addListener(new ContextLoaderListener(context));
+		
+
+		ServletRegistration.Dynamic dispatcher = container.addServlet("dispatcher", new DispatcherServlet(context));
+		
+		dispatcher.setLoadOnStartup(1);
+		dispatcher.addMapping("/");
+		
+	}
+
+}
